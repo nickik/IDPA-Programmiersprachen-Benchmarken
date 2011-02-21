7072a5e3c3cc300486e6d07ceee8414fa6a9ef71
nickik <nickik@gmx.ch>
2011-02-21 21:19:50 +0100

    main doku first

---
+++ clojure/project.clj
@@ -0,0 +1,5 @@
+(defproject binarytree "1.0.0-SNAPSHOT"
+  :description "A binarytree for a benchmark"
+  :aot [binarytrees]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
---
+++ maindoku/build.sh
@@ -0,0 +1,7 @@
+pdflatex main.tex
+makeindex document.glo -s document.ist -t document.glg -o document.gls
+pdflatex main.tex
+rm *.log
+rm *.aux
+rm *.out
+xpdf main.pdf
